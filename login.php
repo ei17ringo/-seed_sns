da8e41b99e34796eb8b5c2a594ca4c71e4ae3b94
ei17ringo <ei17ringo@gmail.com>
2018-01-09 14:42:54 +0800

    認証用のSQL文実行処理

--- login.php
+++ login.php
@@ -1,3 +1,36 @@
+<?php
+
+//DBに接続
+require('dbconnect.php');
+
+// POST送信されていたら
+if (isset($_POST) && !empty($_POST)){
+  // 認証処理
+  try {
+    //メンバーズテーブルでテーブルの中からメールアドレスとパスワードが入力されたものと合致する
+    // データを取得
+    $sql = "SELECT * FROM `members` WHERE `email`=? AND `password`=?";
+
+    //SQL文実行
+    // パスワードは、入力されたものを暗号化した上で使用する
+    $data = array($_POST["email"],sha1($_POST["password"]));
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute($data);
+
+    //1行取得
+    $member = $stmt->fetch(PDO::FETCH_ASSOC);
+    echo "<pre>";
+    var_dump($member);
+    echo "</pre>";
+    
+  } catch (Exception $e) {
+    
+  }
+
+
+}
+
+?>
 <!DOCTYPE html>
 <html lang="ja">
   <head>
