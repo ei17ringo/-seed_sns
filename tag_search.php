f05ef668195da295fc3baef12766ab3241608709
ei17ringo <ei17ringo@gmail.com>
2018-02-01 15:13:04 +0800

    指定したタグを含んだ一覧の表示（SQLのみ） ※SQL実行部分、表示部分は宿題

---
+++ tag_search.php
@@ -0,0 +1,106 @@
+<?php
+  require("dbconnect.php");
+
+  $tag_id = $_GET["tag_id"];
+
+  //tag名の取得
+  $get_tag_sql = "";
+
+  //SQL実行
+
+  //フェッチ
+
+  //tagを含んだ一覧の取得
+  // SELECT `tweets`.*,`members`.`nick_name`,`members`.`picture_path` 
+  // FROM `tweets` INNER JOIN `tweet_tags` 
+  // ON `tweets`.`tweet_id` = `tweet_tags`.`tweet_id`
+  // INNER JOIN `members` ON `tweets`.`member_id` = `members`.`member_id`
+  // WHERE `tag_id` = 4
+
+  $tag_search_sql = "";
+  //SQL実行
+
+  //フェッチ
+
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>SeedSNS</title>
+
+    <!-- Bootstrap -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet">
+    <link href="assets/css/form.css" rel="stylesheet">
+    <link href="assets/css/timeline.css" rel="stylesheet">
+    <link href="assets/css/main.css" rel="stylesheet">
+
+  </head>
+  <body>
+  <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <div class="navbar-header page-scroll">
+              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                  <span class="sr-only">Toggle navigation</span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="index.html"><span class="strong-title"><i class="fa fa-twitter-square"></i> Seed SNS</span></a>
+          </div>
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul class="nav navbar-nav navbar-right">
+                <li><a href="logout.php">ログアウト</a></li>
+              </ul>
+          </div>
+          <!-- /.navbar-collapse -->
+      </div>
+      <!-- /.container-fluid -->
+  </nav>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 col-md-offset-3 content-margin-top">
+        <h4>#Cebuの検索結果</h4>
+        <a href="index.php">&laquo;&nbsp;一覧へ戻る</a>
+        <div class="msg">
+          <img src="http://c85c7a.medialib.glogster.com/taniaarca/media/71/71c8671f98761a43f6f50a282e20f0b82bdb1f8c/blog-images-1349202732-fondo-steve-jobs-ipad.jpg" width="48" height="48">
+          <p>
+            つぶやき４<span class="name"> (Seed kun) </span>
+            [<a href="#">Re</a>]
+          </p>
+          <p class="day">
+            <a href="view.html">
+              2016-01-28 18:04
+            </a>
+          </p>
+        </div>
+        <div class="msg">
+          <img src="http://c85c7a.medialib.glogster.com/taniaarca/media/71/71c8671f98761a43f6f50a282e20f0b82bdb1f8c/blog-images-1349202732-fondo-steve-jobs-ipad.jpg" width="48" height="48">
+          <p>
+            つぶやき４<span class="name"> (Seed kun) </span>
+            [<a href="#">Re</a>]
+          </p>
+          <p class="day">
+            <a href="view.html">
+              2016-01-28 18:04
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="assets/js/jquery-3.1.1.js"></script>
+    <script src="assets/js/jquery-migrate-1.4.1.js"></script>
+    <script src="assets/js/bootstrap.js"></script>
+  </body>
+</html>
